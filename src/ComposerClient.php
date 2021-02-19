89af4a673260751c60afa425d973dbf7683b4b09
Andrey Tronov <newtronov@gmail.com>
2021-02-19 17:38:00 +0300

    fix php 5.3.0

--- src/ComposerClient.php
+++ src/ComposerClient.php
@@ -3,12 +3,19 @@
 
 namespace tronovav\GeoIP2Update;
 
-
+/**
+ * Updates databases through a call from Composer.
+ * Class ComposerClient
+ * @package tronovav\GeoIP2Update
+ */
 class ComposerClient
 {
-    public static function run($event){
+    /**
+     * @param \Composer\Script\Event $event
+     */
+    public static function run(\Composer\Script\Event $event){
         $extra = $event->getComposer()->getPackage()->getExtra();
-        $params = isset($extra[__METHOD__]) ? $extra[__METHOD__] : [];
+        $params = isset($extra[__METHOD__]) ? $extra[__METHOD__] : array();
         $client = new Client($params);
         $client->run();
         fwrite(\STDOUT, implode("\n",array_merge($client->updated(),$client->errors())));
