2f190940c4017db870baa20c91aee344bd86dae2
Andrey Tronov <newtronov@gmail.com>
2021-03-16 22:29:44 +0300

    Code normalization.

--- src/Client.php
+++ src/Client.php
@@ -51,8 +51,8 @@ class Client
     public $tmpDir;
 
     private $urlApi = 'https://download.maxmind.com/app/geoip_download';
-    private $updated =array();
-    private $errors =array();
+    private $updated = array();
+    private $errors = array();
     private $remoteEditions = array(
         'GeoLite2-ASN',
         'GeoIP2-ASN',
@@ -73,7 +73,7 @@ class Client
     {
         $thisClass = new \ReflectionClass($this);
         foreach ($params as $key => $value)
-            if($thisClass->hasProperty($key) && $thisClass->getProperty($key)->isPublic())
+            if ($thisClass->hasProperty($key) && $thisClass->getProperty($key)->isPublic())
                 $this->$key = $value;
             else
                 $this->errors[] = "Parameter \"{$key}\" does not exist.";
@@ -82,7 +82,8 @@ class Client
     /**
      * @return array
      */
-    public function updated(){
+    public function updated()
+    {
         return $this->updated;
     }
 
@@ -90,63 +91,66 @@ class Client
      * Critical update errors.
      * @return array
      */
-    public function errors(){
+    public function errors()
+    {
         return $this->errors;
     }
 
     /**
      * Database update launcher.
      */
-    public function run(){
+    public function run()
+    {
 
-        $this->tmpDir = !empty($this->tmpDir) ?$this->tmpDir : sys_get_temp_dir();
+        $this->tmpDir = !empty($this->tmpDir) ? $this->tmpDir : sys_get_temp_dir();
 
-        if(!is_dir($this->tmpDir) || !is_writable($this->tmpDir))
-            $this->errors[] = sprintf("Temporary directory %s.",(empty($this->tmpDir) ? "not specified" : "{$this->tmpDir} is not writable"));
+        if (!is_dir($this->tmpDir) || !is_writable($this->tmpDir))
+            $this->errors[] = sprintf("Temporary directory %s.", (empty($this->tmpDir) ? "not specified" : "{$this->tmpDir} is not writable"));
 
-        if(!is_dir($this->dir) || !is_writable($this->dir))
-            $this->errors[] = sprintf("Destination directory %s.",(empty($this->dir) ? "not specified" : "$this->dir is not writable"));
+        if (!is_dir($this->dir) || !is_writable($this->dir))
+            $this->errors[] = sprintf("Destination directory %s.", (empty($this->dir) ? "not specified" : "$this->dir is not writable"));
 
-        if(empty($this->type) || !array_key_exists($this->type,$this->remoteTypes))
-            $this->errors[] = sprintf("Database type %s.",(empty($this->type) ? "not specified" : "$this->type does not exist"));
+        if (empty($this->type) || !array_key_exists($this->type, $this->remoteTypes))
+            $this->errors[] = sprintf("Database type %s.", (empty($this->type) ? "not specified" : "$this->type does not exist"));
 
-        if(empty($this->license_key))
+        if (empty($this->license_key))
             $this->errors[] = "You must specify your license_key https://support.maxmind.com/account-faq/license-keys/where-do-i-find-my-license-key/";
 
-        $editionsForUpdate = array_intersect($this->editions,$this->remoteEditions);
-        if(empty($editionsForUpdate))
+        $editionsForUpdate = array_intersect($this->editions, $this->remoteEditions);
+        if (empty($editionsForUpdate))
             $this->errors[] = "No revision names are specified for the update.";
 
-        if(!empty($this->errors))
+        if (!empty($this->errors))
             return;
 
-        foreach ($editionsForUpdate as $edition){
+        foreach ($editionsForUpdate as $edition) {
             $this->updateEdition($edition);
         }
     }
 
-    private function updateEdition($editionId){
+    private function updateEdition($editionId)
+    {
         $newFileRequestHeaders = $this->request(array(
             'edition_id' => $editionId
         ));
 
-        if(empty($newFileRequestHeaders['content-disposition'])){
+        if (empty($newFileRequestHeaders['content-disposition'])) {
             $this->errors[] = "{$editionId}.{$this->type} not found in maxmind.com";
             return;
         }
-        preg_match('/filename=(?<attachment>[\w.\d-]+)$/' ,$newFileRequestHeaders['content-disposition'][0],$matches);
-        $newFileName = $this->tmpDir.DIRECTORY_SEPARATOR.$matches['attachment'];
+        preg_match('/filename=(?<attachment>[\w.\d-]+)$/', $newFileRequestHeaders['content-disposition'][0], $matches);
+        $newFileName = $this->tmpDir . DIRECTORY_SEPARATOR . $matches['attachment'];
 
         $remoteFileLastModified = date_create($newFileRequestHeaders['last-modified'][0])->getTimestamp();
         $localFileLastModified =
-            is_file($this->dir.DIRECTORY_SEPARATOR.$editionId.'.'.$this->type.'.last-modified') ?
-                (int)file_get_contents($this->dir.DIRECTORY_SEPARATOR.$editionId.'.'.$this->type.'.last-modified') : 0;
+            is_file($this->dir . DIRECTORY_SEPARATOR . $editionId . '.' . $this->type . '.last-modified') ?
+                (int)file_get_contents($this->dir . DIRECTORY_SEPARATOR . $editionId . '.' . $this->type . '.last-modified') : 0;
 
-        $oldFileName = $this->dir.DIRECTORY_SEPARATOR.$editionId.'.'.$this->type;
+        $oldFileName = $this->dir . DIRECTORY_SEPARATOR . $editionId . '.' . $this->type;
 
-        if(!is_file($oldFileName) || $remoteFileLastModified !== $localFileLastModified){
+        if (!is_file($oldFileName) || $remoteFileLastModified !== $localFileLastModified) {
 
-            if(is_file($newFileName))
+            if (is_file($newFileName))
                 unlink($newFileName);
 
             $this->request(array(
@@ -157,21 +161,20 @@ class Client
             $phar = new \PharData($newFileName);
             $phar->extractTo($this->tmpDir, null, true);
 
-            $iterator = new \FilesystemIterator(substr($newFileName,0,-7));
+            $iterator = new \FilesystemIterator(substr($newFileName, 0, -7));
 
             foreach ($iterator as $fileIterator)
-                if($fileIterator->isFile() && $fileIterator->getExtension() === $this->type)
-                    rename($fileIterator->getPathname(),$this->dir.DIRECTORY_SEPARATOR.$fileIterator->getFilename());
+                if ($fileIterator->isFile() && $fileIterator->getExtension() === $this->type)
+                    rename($fileIterator->getPathname(), $this->dir . DIRECTORY_SEPARATOR . $fileIterator->getFilename());
                 else
                     unlink($fileIterator->getPathname());
 
             rmdir($iterator->getPath());
             unlink($newFileName);
 
-            file_put_contents($this->dir.DIRECTORY_SEPARATOR.$editionId.'.'.$this->type.'.last-modified',$remoteFileLastModified);
+            file_put_contents($this->dir . DIRECTORY_SEPARATOR . $editionId . '.' . $this->type . '.last-modified', $remoteFileLastModified);
             $this->updated[] = "$editionId.{$this->type} has been updated.";
-        }
-        else
+        } else
             $this->updated[] = "$editionId.{$this->type} does not need to be updated.";
     }
 
@@ -179,30 +182,35 @@ class Client
      * @param array $params
      * @return array|void
      */
-    private function request($params = array()){
-        $url = $this->urlApi.'?'.http_build_query(array(
-                'edition_id'=>!empty($params['edition_id']) ? $params['edition_id'] : '',
-                'suffix'=>$this->remoteTypes[$this->type],
+    private function request($params = array())
+    {
+        $url = $this->urlApi . '?' . http_build_query(array(
+                'edition_id' => !empty($params['edition_id']) ? $params['edition_id'] : '',
+                'suffix' => $this->remoteTypes[$this->type],
                 'license_key' => $this->license_key,
             ));
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
+        $ch = curl_init($url);
 
-        if(!empty($params['save_to'])){
-            curl_setopt($ch, CURLOPT_HTTPGET, true);
-            curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HEADER, false);
+        if (!empty($params['save_to'])) {
+
             $fh = fopen($params['save_to'], 'w');
-            curl_setopt($ch, CURLOPT_FILE, $fh);
+
+            curl_setopt_array($ch, array(
+                CURLOPT_HTTPGET => true,
+                CURLOPT_BINARYTRANSFER => true,
+                CURLOPT_HEADER => false,
+                CURLOPT_FILE => $fh,
+            ));
             curl_exec($ch);
             curl_close($ch);
             fclose($fh);
-        }
-        else{
-            curl_setopt($ch, CURLOPT_HEADER, true);
-            curl_setopt($ch, CURLOPT_NOBODY, true);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        } else {
+            curl_setopt_array($ch, array(
+                CURLOPT_HEADER => true,
+                CURLOPT_NOBODY => true,
+                CURLOPT_RETURNTRANSFER => true,
+            ));
             $header = curl_exec($ch);
             curl_close($ch);
             return $this->parseHeaders($header);
@@ -215,7 +223,7 @@ class Client
      */
     private function parseHeaders($header)
     {
-        $lines = explode("\n",$header);
+        $lines = explode("\n", $header);
         $headers = array();
         foreach ($lines as $line) {
             $parts = explode(':', $line, 2);
