23339920f37816948ed35fd509b05a2add2b54f3
Andrey Tronov <newtronov@gmail.com>
2021-06-03 16:45:48 +0300

    Code formatted.

--- src/ComposerClient.php
+++ src/ComposerClient.php
@@ -14,6 +14,7 @@ namespace tronovav\GeoIP2Update;
  * These libraries are included in the Composer assembly
  * and do not need to be included as a dependency when updating databases through Composer.
  */
+
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 /**
@@ -21,21 +22,21 @@ use Symfony\Component\Console\Output\ConsoleOutput;
  * Class ComposerClient
  * @package tronovav\GeoIP2Update
  */
-
 class ComposerClient
 {
     /**
      * Database update launcher via console.
      * @param \Composer\Script\Event $event
      */
-    public static function run(\Composer\Script\Event $event){
+    public static function run(\Composer\Script\Event $event)
+    {
 
         $extra = $event->getComposer()->getPackage()->getExtra();
         $params = isset($extra[__METHOD__]) ? $extra[__METHOD__] : array();
 
-        if(isset($params['dir'])){
-            $params['dir'] = realpath(str_replace('@composer',realpath(dirname(\Composer\Factory::getComposerFile())),$params['dir']));
-            $params['dir'] = str_replace(array('\\','/'),DIRECTORY_SEPARATOR,$params['dir']);
+        if (isset($params['dir'])) {
+            $params['dir'] = realpath(str_replace('@composer', realpath(dirname(\Composer\Factory::getComposerFile())), $params['dir']));
+            $params['dir'] = str_replace(array('\\', '/'), DIRECTORY_SEPARATOR, $params['dir']);
         }
 
         $client = new ComposerConsole($params);
@@ -44,11 +45,11 @@ class ComposerClient
         $output = new ConsoleOutput();
 
         $infoArray = $client->updated();
-        array_walk($infoArray,function ($info) use ($output){
+        array_walk($infoArray, function ($info) use ($output) {
             $output->writeln("<fg=green>$info</>");
         });
         $errorsArray = $client->errors();
-        array_walk($errorsArray,function ($error) use ($output){
+        array_walk($errorsArray, function ($error) use ($output) {
             $output->writeln("<fg=red>$error</>");
         });
     }
